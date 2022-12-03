826114c55950c8e59fc85805ec4df07280683ccd
Chris W <41281925+carpeomnia777@users.noreply.github.com>
2022-12-03 14:19:37 -0500

    Update fizzbuzz4Script.js

--- web115/scripts/fizzbuzz4Script.js
+++ web115/scripts/fizzbuzz4Script.js
@@ -16,14 +16,14 @@ const btn = document.querySelector('#btn');
             var lName = document.getElementById("last_name").value;
             document.getElementById("greeting").innerHTML = `Welcome To CarpeOmnia Consulting, ${fName} ${mInit} ${lName}!`;
             var list=document.getElementById("fizzList");
-            var num = prompt(`How high would you like to count, ${fName}?`);
-            var div1 = prompt(`Please enter a number, ${fName}`);
-            var word1 = prompt(`Please enter a word to go with the first number, ${fName}`);
-            var div2 = prompt(`please enter a second number, ${fName}`);
-            var word2 = prompt(`Please enter a word to go with the second number, ${fName}`);
-            var div3 = prompt(`Please enter a third number, ${fName}`);
-            var word3 = prompt(`Please enter a word to go with the third number, ${fName}`);
-            var wordD = prompt(`Please enter a word for the numbers not divisible by the other chosen numbers, ${fName}`);
+            var num = prompt(`How high would you like to count, ${fName}?`, 140);
+            var div1 = prompt(`Please enter a number, ${fName}` , 3);
+            var word1 = prompt(`Please enter a word to go with the first number, ${fName}` , "click");
+            var div2 = prompt(`please enter a second number, ${fName}`, 5);
+            var word2 = prompt(`Please enter a word to go with the second number, ${fName}` , "clack");
+            var div3 = prompt(`Please enter a third number, ${fName}`, 7);
+            var word3 = prompt(`Please enter a word to go with the third number, ${fName}`, "thunk");
+            var wordD = prompt(`Please enter a word for the numbers not divisible by the other chosen numbers, ${fName}`, "working");
             
             for (let i=1; i<= num; i++){
              var li = document.createElement("li");
