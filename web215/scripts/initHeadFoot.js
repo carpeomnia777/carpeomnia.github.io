91c0acd9af3d171144102488583447648b666885
Chris W <41281925+carpeomnia777@users.noreply.github.com>
2024-02-11 11:15:45 -0500

    Update initHeadFoot.js

--- web215/scripts/initHeadFoot.js
+++ web215/scripts/initHeadFoot.js
@@ -12,6 +12,19 @@ function renderHeader() {
 }
 
 function renderFooter() {
+    var currentPageUrl = window.location.href;
+
+    var pageName = currentPageUrl.substring(currentPageUrl.lastIndexOf('/') + 1);
+
+    
+    var validatorLink = `
+        <a href="https://validator.w3.org/nu/?doc=${encodeURIComponent(currentPageUrl)}" style="text-decoration:none;">
+            <img src="../images/valid-html-blue.png">
+        </a>
+        <a href="https://jigsaw.w3.org/css-validator/validator?uri=${encodeURIComponent(currentPageUrl)}&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en" style="text-decoration:none;">
+            <img src="../images/valid-css-blue.png">
+        </a>`;
+
     document.getElementById('footer').innerHTML = 
     `<hr>
     <nav>
@@ -23,8 +36,7 @@ function renderFooter() {
       <a href="https://jsfiddle.net/user/carpeomnia777/fiddles/">JSFiddle</a> |
       <a href="https://www.linkedin.com/learning/?u=76141674/">LinkedIn Learning</a> |
      </nav> 
-    <a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fcarpeomnia777.github.io%2Fweb215%2Fprojects%2Fbrand" style="text-decoration:none;"><img src="../images/valid-html-blue.png"></a>
-    <a href="https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fcarpeomnia777.github.io%2Fweb215%2Fprojects%2Fbrand&amp;profile=css3svg&amp;usermedium=all&amp;warning=1&amp;vextwarning=&amp;lang=en" style="text-decoration:none;"><img src="../images/valid-css-blue.png"></a>`;
+     ${validatorLink}`;
 }
 
 
